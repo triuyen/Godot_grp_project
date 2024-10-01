64760f66cb177033c49c56e9605e87924b0ebce0
TRIUYEN_TANG <triuyentang@gmail.com>
2024-10-01 14:53:38 +0200

    [WIP] reset root script

--- godot/scripts/root.gd
+++ godot/scripts/root.gd
@@ -1,12 +1,9 @@
 extends Node2D
 
-@onready var current_level = $level_1
 # stats 
 var damage_amount: int = 20
 
 func _ready() -> void:
-	var levelManager = current_level.getmode("levelManager")
-	levelManager.connect()
 	pass
 
 func _process(_delta: float) -> void:
