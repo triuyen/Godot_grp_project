460b056d671a4038e304d491da1526ebdbc0f6f2
TRIUYEN_TANG <triuyentang@gmail.com>
2024-10-01 14:51:27 +0200

    [WIP] seperate the 2 level code the systeme of spawning between levels

--- godot/scripts/root.gd
+++ godot/scripts/root.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+@onready var current_level = $level_1
 # stats 
 var damage_amount: int = 20
 
 func _ready() -> void:
+	var levelManager = current_level.getmode("levelManager")
 	pass
 
 func _process(_delta: float) -> void:
