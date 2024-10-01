d5188201f4dca05dcf847c7afd8fe06124f8467a
MrSebi06 <mihneasebastian06@gmail.com>
2024-10-01 23:03:57 +0200

    Prevent knockback from cancelling

--- godot/scripts/slime.gd
+++ godot/scripts/slime.gd
@@ -3,8 +3,15 @@ extends  CharacterBody2D
 @export var blink_duration: float = 0.3 
 
 var knockback_force = 300
+var is_knocked = false
 
 func _process(delta: float) -> void:
+	if is_knocked:
+		return
+	# move direction
+	pass
+
+func _physics_process(delta: float) -> void:
 	self.move_and_slide()
 	
 
@@ -13,10 +20,12 @@ func _on_hit_taken(hit_position: Vector2) -> void:
 	
 	var hit_direction = (self.global_position - hit_position).normalized()
 	self.velocity = hit_direction * knockback_force
+	is_knocked = true
 	
 	await get_tree().create_timer(0.2).timeout
 	
 	self.velocity = Vector2.ZERO
+	is_knocked = false
 	
 func _start_blinking() -> void:
 	var blink_time = 0.0
