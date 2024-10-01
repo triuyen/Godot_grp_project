2a22ab251d647047eca749fdde427091e1deefa1
MrSebi06 <mihneasebastian06@gmail.com>
2024-10-01 22:37:20 +0200

    Add death animation

--- godot/scripts/hurtbox.gd
+++ godot/scripts/hurtbox.gd
@@ -3,9 +3,7 @@ extends Area2D
 signal on_hit_taken(hit_position: Vector2)
 
 @export var health: HealthComponent
-@export var sprite: AnimatedSprite2D
 @export var invincibility_timer: float = 1.0
-@export var blink_duration: float = 0.3 
 
 var can_be_hit = true
 
@@ -14,12 +12,7 @@ func _on_area_entered(area: Area2D) -> void:
 		if not can_be_hit:
 			return
 		health.take_damage(area.damage)
-		
 		on_hit_taken.emit(area.global_position)
-		
-		if is_instance_valid(sprite):
-			_start_blinking()
-		
 		if invincibility_timer:
 			_start_invincibility()
 
@@ -32,12 +25,3 @@ func _start_invincibility() -> void:
 	can_be_hit = true 
 	set_deferred("monitoring", true)
 	
-
-func _start_blinking() -> void:
-	var blink_time = 0.0
-	while blink_time < blink_duration:
-		sprite.modulate = Color(10.0, 10.0, 10.0, 0.7)
-		await get_tree().create_timer(0.05).timeout
-		sprite.modulate = Color(1.0, 1.0, 1.0)
-		await get_tree().create_timer(0.05).timeout
-		blink_time += 0.1 
--- godot/scripts/slime.gd
+++ godot/scripts/slime.gd
@@ -1,5 +1,7 @@
 extends  CharacterBody2D
 
+@export var blink_duration: float = 0.3 
+
 var knockback_force = 300
 
 func _process(delta: float) -> void:
@@ -7,7 +9,26 @@ func _process(delta: float) -> void:
 	
 
 func _on_hit_taken(hit_position: Vector2) -> void:
+	_start_blinking()
+	
 	var hit_direction = (self.global_position - hit_position).normalized()
 	self.velocity = hit_direction * knockback_force
+	
 	await get_tree().create_timer(0.2).timeout
+	
 	self.velocity = Vector2.ZERO
+	
+func _start_blinking() -> void:
+	var blink_time = 0.0
+	while blink_time < blink_duration:
+		$sprite.modulate = Color(10.0, 10.0, 10.0, 0.7)
+		await get_tree().create_timer(0.05).timeout
+		$sprite.modulate = Color(1.0, 1.0, 1.0)
+		await get_tree().create_timer(0.05).timeout
+		blink_time += 0.1 
+
+func _on_death() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($sprite, "scale", Vector2(), 0.3)
+	tween.tween_callback(queue_free)
+	
