b1b54e293308ebb3f5e3a62ab044042989b95112
MrSebi06 <mihneasebastian06@gmail.com>
2024-10-01 15:19:37 +0200

    Cleanup

---
+++ godot/scripts/hurtbox.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+func _on_area_entered(area: Area2D) -> void:
+	# Damage logic will come here once the health system is implemented
+	pass
+	
--- godot/scripts/slime.gd
+++ godot/scripts/slime.gd
@@ -1,4 +1,1 @@
-ClassDB::register_class<Bullet>()
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+extends  CharacterBody2D
