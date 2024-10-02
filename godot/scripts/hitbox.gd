8e47cd492ea7b6eab333d6a12e97bdf26ad67538
MrSebi06 <mihneasebastian06@gmail.com>
2024-10-02 10:33:23 +0200

    Add die on hit on hitbox

--- godot/scripts/hitbox.gd
+++ godot/scripts/hitbox.gd
@@ -1,3 +1,10 @@
 class_name HitboxComponent extends Area2D
 
+signal _on_dies_on_hit
+
 @export var damage = 100
+@export var dies_on_hit: bool = false
+
+func _on_body_entered(body: Node2D) -> void:
+	if dies_on_hit:
+		_on_dies_on_hit.emit()
